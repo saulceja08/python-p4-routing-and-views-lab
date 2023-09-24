ebb597014929d3c976327811fd073536b8436743
Saul Ceja <saulceja08@gmail.com>
2023-09-24 00:45:08 -0700

    pytest

--- server/app.py
+++ server/app.py
@@ -6,3 +6,38 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:text>')
+def print_string(text):
+    print(text)
+    return text
+
+@app.route('/count/<int:number>')
+def count(number):
+    number_list = [str(i) for i in range(number)]
+    return '\n'.join(number_list) + '\n'
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def do_math(num1, operation, num2):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        if num2 == 0:
+            return "Error: Division by zero"
+        result = num1 / num2
+    elif operation == '%':
+        if num2 == 0:
+            return "Error: Modulo by zero"
+        result = num1 % num2
+    else:
+        return "Invalid operation"
+
+    return str(result)
